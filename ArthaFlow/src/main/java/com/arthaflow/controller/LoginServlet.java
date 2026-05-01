3ecf9cf00440acb0e10835c88d49eea1ecf5f5a2
prakash69-code <neupaneprakash9999@gmail.com>
2026-05-01 09:19:08 +0545

    feat: implement login servlet with cookie and session

--- ArthaFlow/src/main/java/com/arthaflow/controller/LoginServlet.java
+++ ArthaFlow/src/main/java/com/arthaflow/controller/LoginServlet.java
@@ -1,4 +1,43 @@
 package com.arthaflow.controller;
 
-public class LoginServlet {
+import com.arthaflow.model.User;
+import com.arthaflow.service.UserService;
+import com.arthaflow.util.CookieUtil;
+import com.arthaflow.util.SessionService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+    UserService userservice = new UserService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/jsp/user/login.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+
+        User user = userservice.authenticateUser(email, password);
+        if(user != null){
+            SessionService.setUserSession(req, user);
+            CookieUtil.setUserCookie(resp, email);
+
+            if("ADMIN".equals(user.getRole())){
+                resp.sendRedirect(req.getContextPath() + "/admin/dashboard");
+            }else {
+                resp.sendRedirect(req.getContextPath() + "/user/dashboard");
+            }
+        } else {
+            req.setAttribute("Error Message","Invalid Email or Password");
+            req.getRequestDispatcher("jsp/user/login.jsp").forward(req, resp);
+        }
+
+    }
 }
---
+++ ArthaFlow/src/main/webapp/jsp/user/login.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: neupa
+  Date: 4/28/2026
+  Time: 10:55 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Login Page</title>
+</head>
+<body>
+
+</body>
+</html>
