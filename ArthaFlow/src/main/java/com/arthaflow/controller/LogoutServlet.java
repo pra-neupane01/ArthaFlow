6637427ff8fe5e10f4a9d4448283262e23795c23
prajwalkarna <pranilkarna101@gmail.com>
2026-05-02 14:52:19 +0545

    fix: removed unnecessary @WebServlet annotations

--- ArthaFlow/src/main/java/com/arthaflow/controller/LogoutServlet.java
+++ ArthaFlow/src/main/java/com/arthaflow/controller/LogoutServlet.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/logout")
+//@WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
 
     @Override
