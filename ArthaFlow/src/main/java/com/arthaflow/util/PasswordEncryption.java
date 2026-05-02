6bbbb510c6a9814f73c3f03a1c411a7221e4cc59
prajwalkarna <pranilkarna101@gmail.com>
2026-05-02 18:55:42 +0545

    "fix: remove unused imports from PasswordEncryption"

--- ArthaFlow/src/main/java/com/arthaflow/util/PasswordEncryption.java
+++ ArthaFlow/src/main/java/com/arthaflow/util/PasswordEncryption.java
@@ -1,16 +1,13 @@
 package com.arthaflow.util;
-import com.arthaflow.dao.UserDAO;
-import com.arthaflow.model.User;
 import org.mindrot.jbcrypt.BCrypt;
 
 public class PasswordEncryption {
-    private static User user = new User();
 
     public static String hashPassword(String password){
         String salt = BCrypt.gensalt(12);
         String hashPassword =BCrypt.hashpw(password, salt);
         return hashPassword;
-            }
+    }
 
     public static boolean verifyPassword(String password, String hashedPassword){
         return BCrypt.checkpw(password, hashedPassword);
